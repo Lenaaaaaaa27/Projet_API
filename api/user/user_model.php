b78f28255dc4808640bc83a584a6d66f38c8b00e
Lena <vincennes.lena@gmail.com>
2023-12-05 16:43:33 +0100

    Fix UserModel

--- api/user/user_model.php
+++ api/user/user_model.php
@@ -1,26 +1,30 @@
 <?php 
 
-/**
- *  @var int
- */
+
+class UserModel{
+    /**
+     *  @var int
+     */
     public $id;
 
-/**
- *  @var int
- */
-    public $role;
+    /**
+     *  @var int
+     */
+        public $role;
 
-public function __construct($id, $role){
-    $this->id = $id;
-    $this->role = $role;
+    public function __construct($id = NULL, $role){
+        $this->id = $id;
+        $this->role = $role;
+    }
+
+    // Au cas où on met les attributs en private
+
+    /* public function get_id($id){
+        return $this->id;
+    }
+
+    public function get_role($role){
+        return $this->role;
+    } */
 }
 
-// Au cas où on met les attributs en private
-
-/* public function get_id($id){
-    return $this->id;
-}
-
-public function get_role($role){
-    return $this->role;
-} */
