bfd61ac71695036bf12021c5858ccf448f8db7e6
Lena <vincennes.lena@gmail.com>
2023-12-05 14:52:51 +0100

    Edit Index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,21 @@
 <?php 
-// index.php
+// Skipper les warnings, pour la production (vos exceptions devront être gérées proprement)
+error_reporting(E_ERROR | E_PARSE);
+
+// le contenu renvoyé par le serveur sera du JSON
+header("Content-Type: application/json; charset=utf8");
+// Autorise les requêtes depuis localhost
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS,PATCH');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+// On récupère l'URI de la requête et on le découpe en fonction des / 
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri ); // On obtient un tableau de la forme ['index.php', 'todos', '1']
+
+// Si on a moins de 3 éléments dans l'URI, c'est que l'on est sur l'index de l'API
+if (sizeof($uri) < 3) {
+    header("HTTP/1.1 200 OK");
+    echo '{"message": "Welcome to the API"}';
+    exit();
+}
