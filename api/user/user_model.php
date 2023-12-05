24553391e2148ea73698fdc30aa3be1f5ca2b2a3
Lena <vincennes.lena@gmail.com>
2023-12-05 16:25:10 +0100

    Create user_model.php

---
+++ api/user/user_model.php
@@ -0,0 +1,26 @@
+<?php 
+
+/**
+ *  @var int
+ */
+    public $id;
+
+/**
+ *  @var int
+ */
+    public $role;
+
+public function __construct($id, $role){
+    $this->id = $id;
+    $this->role = $role;
+}
+
+// Au cas où on met les attributs en private
+
+/* public function get_id($id){
+    return $this->id;
+}
+
+public function get_role($role){
+    return $this->role;
+} */
