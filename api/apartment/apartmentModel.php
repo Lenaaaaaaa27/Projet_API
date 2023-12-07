080b02fedf7e5127ae6b1c4d3abe0ea4eb030ad4
Wallblue <nrb.busquet@gmail.com>
2023-12-07 23:10:00 +0100

    Add getApartment() method in repository

--- api/apartment/apartmentModel.php
+++ api/apartment/apartmentModel.php
@@ -1,19 +1,21 @@
 <?php
 class ApartmentModel{
+    public $id;
     public $area; //In square meters
     public $capacity; //Number of people who can live in the apartment
     public $address;
-    public $disponibilty; //False if occupied, True if free
+    public $disponibility; //False if occupied, True if free
     public $price;
     public $owner; //Name of the owner
 
-    public function __construct($address, $area, $owner, $capacity, $price, $disponibilty){
-        $this->area;
-        $this->capacity;
-        $this->address;
-        $this->disponibilty;
-        $this->price;
-        $this->owner;
+    public function __construct($id, $address, $area, $owner, $capacity, $price, $disponibility){
+        $this->id = $id;
+        $this->area = $area;
+        $this->capacity = $capacity;
+        $this->address = $address;
+        $this->disponibility = $disponibility;
+        $this->price = $price;
+        $this->owner = $owner;
     }
 }
 ?>
--- api/apartment/apartmentRepository.php
+++ api/apartment/apartmentRepository.php
@@ -29,12 +29,44 @@ class ApartmentRepository{
     public function newApartment(ApartmentModel $apart): ApartmentModel{
         $query = "INSERT INTO APARTMENT (area, capacity, address, disponibility, price, owner) 
                                 VALUES ($1, $2, $3, $4, $5, $6)";
-        $args = [$this->area, $this->capacity, $this->address, $this->disponibilty, $this->price, $this->owner];
+        $args = [$this->area, $this->capacity, $this->address, $this->disponibility, $this->price, $this->owner];
 
         $res = $this->query($query, $args);
         $created = pg_fetch_assoc($res);
 
         return new ApartmentModel($created['id'], $created['address'], $created['area'], $created['owner'], $created['capacity'], $created['price'], $created['disponibility']);
+    }
+
+    public function getApartment($id): ApartmentModel{
+        $query = "SELECT 
+                    address, area,
+                    capacity, disponibility,
+                    price, owner
+                  FROM APARTMENT WHERE id = $1";
+        
+        $res = $this->query($query, $this->id);
+        $res = pg_fetch_assoc($res);
+        if($res == NULL){
+            throw new Exception("Apartment not found.");
+        }
+
+        return new ApartmentModel($res['id'], $res['address'], $res['area'], $res['owner'], $res['capacity'], $res['price'], $res['disponibility']);
+    }
+
+    public function getApartments(): array{
+
+    }
+
+    public function getFreeApartments(): array{
+
+    }
+
+    public function updateApartment(): ApartmentModel{
+
+    }
+
+    public function deleteApartment(): void{
+
     }
 }
 ?>
