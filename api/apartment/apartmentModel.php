4df8463669a4af15d6bd1e9cf25dbad8e69d5361
Wallblue <nrb.busquet@gmail.com>
2023-12-07 21:34:45 +0100

    Patch : Completed Apartment Model construct

--- api/apartment/apartmentModel.php
+++ api/apartment/apartmentModel.php
@@ -1,14 +1,19 @@
 <?php
 class apartmentModel{
-    public $area;
-    public $capacity;
+    public $area; //In square meters
+    public $capacity; //Number of people who can live in the apartment
     public $address;
-    public $disponibilty;
+    public $disponibilty; //False if occupied, True if free
     public $price;
-    public $owner;
+    public $owner; //Name of the owner
 
-    public function __construct($address){
-
+    public function __construct($address, $area, $owner, $capacity, $price, $disponibilty){
+        $this->area;
+        $this->capacity;
+        $this->address;
+        $this->disponibilty;
+        $this->price;
+        $this->owner;
     }
 }
 ?>
