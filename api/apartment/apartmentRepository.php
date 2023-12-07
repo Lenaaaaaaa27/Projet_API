09bfaa35a0ce08f3ee5f2b1a64fe478ee69e220f
Wallblue <nrb.busquet@gmail.com>
2023-12-07 15:27:02 +0100

    Add __construct method in apartmentRepository class

--- api/apartment/apartmentRepository.php
+++ api/apartment/apartmentRepository.php
@@ -1,3 +1,15 @@
 <?php
+class apartmentRepository{
+    private db;
 
+    public function __construct(){
+        try{
+            $this->db = pg_connect("host=database port=5432 dbname=rent_db user=rental password=password");
+            if($this->db == NULL)
+                throw new Exception("Could not connect to database");
+        }catch(Exception $e){
+            throw new Exception("Database connection failed :" . $e->getMessage());
+        }
+    }
+}
 ?>
