c50d9483db6e91bdfc3bcbc0d12efe6699c4e06d
Wallblue <nrb.busquet@gmail.com>
2023-12-10 15:45:09 +0100

    Added Router method in Apartment controller

--- api/apartment/apartmentController.php
+++ api/apartment/apartmentController.php
@@ -5,5 +5,41 @@ class ApartmentController{
     public function __construct(){
         $this->service = new ApartmentService();
     }
+
+    public function router($req, $res){
+        switch($req->getMethod()){
+            case 'GET':
+                if($req->getPathAt(3) === 'free')
+                    $res->setContent($this->service->getFreeApartments());
+                elseif($req->getPathAt(3) !== '')
+                    $res->setContent($this->service->getApartment($req->getPathAt(3)));
+                else
+                    $res = json_encode($this->service->getApartments());
+                break;
+
+            case 'POST':
+                $result = $this->service->createApartment($req->getBody());
+                $res->setContent($result);
+                break;
+
+            case 'PATCH':
+                if($req->getPathAt(3) === '')
+                    throw new Exception('Please provide the ID of the apartment you want to modify.');
+
+                $result = $this->service->modifyApartment($req->getPathAt(3), $res->getBody());
+                $res->setContent($result);
+                break;
+
+            case 'DELETE':
+                if($req->getPathAt(3) === '')
+                    throw new Exception('Please provide the ID of the apartment you want to modify.');
+
+                $this->service->deleteApartment($req->getPathAt(3));
+                $res->setMessage('Successfully deleted Apartment id' . $req->getPathAt(3), 200);
+                break;
+        }
+
+        return $res;
+    }
 }
 ?>
