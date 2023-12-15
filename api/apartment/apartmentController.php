bf8d8e805ca52b6245256c811c3afd1bed9adf9f
Wallblue <nrb.busquet@gmail.com>
2023-12-15 18:46:51 +0100

    Added routes for GET owner and PATCH switch for apartments

--- api/apartment/apartmentController.php
+++ api/apartment/apartmentController.php
@@ -15,12 +15,24 @@ class ApartmentController{
     public function dispatch(Request $req, Response $res): void{
         switch($req->getMethod()){
             case 'GET':
-                if($req->getPathAt(4) === 'free')
-                    $res->setContent($this->service->getFreeApartments());
-                elseif($req->getPathAt(4) !== '')
-                    $res->setContent($this->service->getApartment($req->getPathAt(4)));
-                else
-                    $res->setContent($this->service->getApartments());
+                switch($req->getPathAt(4)){
+                    case 'free':
+                        $res->setContent($this->service->getFreeApartments());
+                        break;
+
+                    case 'owner':
+                        if(!isset($_GET['id']))
+                            throw new BadRequestException('Please provide ID of the owner as an argument.');
+                        $res->setContent($this->service->getApartmentsByOwner($_GET['id']));
+                        break;
+
+                    case '':
+                        $res->setContent($this->service->getApartments());
+                        break;
+
+                    default:
+                        $res->setContent($this->service->getApartment($req->getPathAt(4)));
+                }
                 break;
 
             case 'POST':
@@ -32,7 +44,11 @@ class ApartmentController{
                 if($req->getPathAt(4) === '')
                     throw new BadRequestException('Please provide the ID of the apartment you want to modify.');
 
-                $result = $this->service->modifyApartment($req->getPathAt(4), $req->getBody());
+                if($req->getPathAt(5) == 'switch')
+                    $result = $this->service->switchDisponibityOn($req->getPathAt(4));
+                else
+                    $result = $this->service->modifyApartment($req->getPathAt(4), $req->getBody());
+
                 $res->setContent($result);
                 break;
 
