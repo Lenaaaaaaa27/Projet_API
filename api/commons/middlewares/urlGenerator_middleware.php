f415c15d2ed6616e47d1e2f9828e65e5251ab783
Wallblue <nrb.busquet@gmail.com>
2023-12-18 03:31:42 +0100

    Recoded urlGenerator_middleware in a better way

--- api/commons/middlewares/urlGenerator_middleware.php
+++ api/commons/middlewares/urlGenerator_middleware.php
@@ -1,15 +1,39 @@
 <?php
 require_once 'commons/request.php';
 require_once 'commons/response.php';
-/*
 
-L'URL à la fin doit avoir cette gueule :
+function urlGeneratorMiddleware(Request $req, Response $res, $content){
+    if($content == NULL || $req->getMethod() == 'DELETE') return; //Get out if you don't have content !!
 
-http://localhost:8083/index.php/$req->getPathAt(2)/$req->getPathAt(3)/$id
+    $testingObject = is_array($content) ? $content[0] : $content; //We need a non-array object for the next tests
 
-*/
+    if(isset($testingObject->mail)){
+        $firstAttr = 'ownedApartments';
+        $secondAttr = 'madeReservations';
+    }
+    if(isset($testingObject->start_date)){
+        $firstAttr = 'renter';
+        $secondAttr = 'apartment';
+    }
+    if(isset($testingObject->area)){
+        $firstAttr = 'owner';
+        $secondAttr = 'linkedReservations';
+    }
 
-function makeURLFromArray($arr, Request $req): string{
+    if(is_array($content)){ //We create the links depending of the attributes we selected
+        foreach($content as $object){
+            generateAttributeLink($object, $firstAttr, $req);
+            generateAttributeLink($object, $secondAttr, $req);
+        }
+    }else{
+        generateAttributeLink($content, $firstAttr, $req);
+        generateAttributeLink($content, $secondAttr, $req);
+    }
+
+    $res->setContent($content); //Then we set the content
+}
+
+function makeURLFromArray($arr, Request $req): string{ //Create the URL from the array received 
     if(isset($arr["mail"])) $type = 'user';
     if(isset($arr["area"])) $type = 'apartment';
     if(isset($arr["start_date"])) $type = 'reservation';
@@ -18,53 +42,15 @@ function makeURLFromArray($arr, Request $req): string{
     return $url;
 }
 
-function urlGeneratorMiddleware(Request $req, Response $res, $content){
-    if($content == NULL || $req->getMethod() == 'DELETE') return;
-    $objectType = $req->getPathAt(3);
-    switch($objectType){
-        case 'user':
-            if(is_array($content))
-                foreach($content as $user)
-                    generateUserLinks($user, $req);
-            else
-                generateUserLinks($content, $req);
+function generateAttributeLink($content, $attr, Request $req){ //Calling previous function the right amount of times and with the right informations
+    switch($attr){
+        case 'ownedApartments': case 'madeReservations': case 'linkedReservations': //These attr contain multiple objects
+            for($i = 0; $i < count($content->$attr); $i++)
+                $content->$attr[$i]['url'] = makeURLFromArray($content->$attr[$i], $req);
             break;
-
-        case 'reservation':
-            if(is_array($content))
-                foreach($content as $reservation)
-                    generateReservationLinks($reservation, $req);
-            else
-                generateReservationLinks($content, $req);
+        case 'renter': case 'apartment': case 'owner': //These attr contain an unique object
+            $content->$attr['url'] = makeURLFromArray($content->$attr, $req);
             break;
-
-        case 'apartment':
-            if(is_array($content))
-                foreach($content as $apartment)
-                    generateApartmentLinks($apartment, $req);
-            else
-                generateApartmentLinks($content, $req);
-            break;
-            
+        }
     }
-    $res->setContent($content);
-}
-
-function generateUserLinks($content, Request $req): void{
-    for($i = 0; $i < count($content->ownedApartments); $i++)
-        $content->ownedApartments[$i]['url'] = makeURLFromArray($content->ownedApartments[$i], $req);
-    for($i = 0; $i < count($content->madeReservations); $i++)
-        $content->madeReservations[$i]['url'] = makeURLFromArray($content->madeReservations[$i], $req);
-}
-
-function generateReservationLinks($content, Request $req): void{
-    $content->renter['url'] = makeURLFromArray($content->renter, $req);
-    $content->apartment['url'] = makeURLFromArray($content->apartment, $req);
-}
-
-function generateApartmentLinks($content, Request $req): void{
-    $content->owner['url'] = makeURLFromArray($content->owner, $req);
-    for($i = 0; $i < count($content->linkedReservations); $i++)
-        $content->linkedReservations[$i]['url'] = makeURLFromArray($content->linkedReservations[$i], $req);
-}
 ?>
